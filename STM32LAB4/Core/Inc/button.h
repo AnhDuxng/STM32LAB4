e16bf15de10cbf46dc168b5997ae0d8be5eea803
AnhDuxng <dung.ho265@hcmut.edu.vn>
2023-11-27 12:35:58 +0700

    Init Lab4
    scheduler

---
+++ STM32LAB4/Core/Inc/button.h
@@ -0,0 +1,18 @@
+/*
+ * button.h
+ *
+ *  Created on: Nov 27, 2023
+ *      Author: anhdu
+ */
+
+#ifndef INC_BUTTON_H_
+#define INC_BUTTON_H_
+
+#include "main.h"
+
+#define NORMAL_STATE GPIO_PIN_SET
+#define PRESSED_STATE GPIO_PIN_RESET
+
+int BUTTON_PRESSED();
+void getKeyInput();
+#endif /* INC_BUTTON_H_ */
